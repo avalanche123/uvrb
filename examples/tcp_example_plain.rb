3d3fdf2f1ee5c829869148413bec6e606aaf099e
Bulat Shakirzyanov <mallluhuct@gmail.com>
2012-04-12 00:47:57 -0700

    add plain ruby tcp server implementation for comparison

---
+++ examples/tcp_example_plain.rb
@@ -0,0 +1,23 @@
+require 'socket'
+require 'thread'
+require 'timeout'
+
+server = TCPServer.new(10000)
+server.listen(128)
+
+Timeout.timeout(50) do
+  loop do
+    Thread.start(server.accept) do |client|
+      # while (input = client.gets)
+      #   puts input
+      # end
+      client.puts "HTTP/1.1 200 OK\r\n"
+      client.puts "Content-Type: text/plain\r\n"
+      client.puts "Content-Length: 12\r\n"
+      client.puts "\r\n"
+      client.puts "hello world\n"
+      client.close
+    end
+  end
+end
+
