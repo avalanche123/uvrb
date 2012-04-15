34a7f28f541c6f67fe28b60b87195e780da726d9
Bulat Shakirzyanov <mallluhuct@gmail.com>
2012-04-14 21:48:02 -0700

    fix typecasting

--- lib/uv/net.rb
+++ lib/uv/net.rb
@@ -14,16 +14,16 @@ module UV
     def get_ip_and_port(sockaddr, len=nil)
       if sockaddr[:sa_family] == Socket::Constants::AF_INET6
         len ||= Socket::Constants::INET6_ADDRSTRLEN
-        sockaddr_in6 = UV::SockaddrIn6.new(sockaddr)
+        sockaddr_in6 = UV::SockaddrIn6.new(sockaddr.pointer)
         ip_ptr = FFI::MemoryPointer.new(:char, len)
         UV.ip6_name(sockaddr_in6, ip_ptr, len)
-        port = UV.ntohs(sockaddr_in6[:port])
+        port = UV.ntohs(sockaddr_in6[:sin6_port])
       else
         len ||= Socket::Constants::INET_ADDRSTRLEN
-        sockaddr_in = UV::SockaddrIn.new(sockaddr)
+        sockaddr_in = UV::SockaddrIn.new(sockaddr.pointer)
         ip_ptr = FFI::MemoryPointer.new(:char, len)
         UV.ip4_name(sockaddr_in, ip_ptr, len)
-        port = UV.ntohs(sockaddr_in[:port])
+        port = UV.ntohs(sockaddr_in[:sin_port])
       end
       [ip_ptr.read_string, port]
     end
