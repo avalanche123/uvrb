76361b4315f165fa25de0e4c30b8645a372822dd
Bulat Shakirzyanov <mallluhuct@gmail.com>
2012-05-20 00:21:46 -0700

    add prepare spec

---
+++ spec/uv/prepare_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe UV::Prepare do
+  let(:handle_name) { :prepare }
+  let(:loop) { double() }
+  let(:pointer) { double() }
+  subject { UV::Prepare.new(loop, pointer) }
+
+  it_behaves_like 'a handle'
+
+  describe "#start" do
+    it "requires a block" do
+      expect { subject.start }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.prepare_start" do
+      UV.should_receive(:prepare_start).with(pointer, subject.method(:on_prepare))
+
+      subject.start {}
+    end
+  end
+
+  describe "#stop" do
+    it "calls UV.prepare_stop" do
+      UV.should_receive(:prepare_stop).with(pointer)
+
+      subject.stop
+    end
+  end
+end
