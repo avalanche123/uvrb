a46a52f4a8e43c8bc61b97765c43268eacf90529
Bulat Shakirzyanov <mallluhuct@gmail.com>
2012-05-20 00:12:42 -0700

    add idle spec

---
+++ spec/uv/idle_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe UV::Idle do
+  let(:handle_name) { :idle }
+  let(:loop) { double() }
+  let(:pointer) { double() }
+  subject { UV::Idle.new(loop, pointer) }
+
+  it_behaves_like 'a handle'
+
+  describe "#start" do
+    it "requires a block" do
+      expect { subject.start }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.idle_start" do
+      UV.should_receive(:idle_start).with(pointer, subject.method(:on_idle))
+
+      subject.start {}
+    end
+  end
+
+  describe "#stop" do
+    it "calls UV.idle_stop" do
+      UV.should_receive(:idle_stop).with(pointer)
+
+      subject.stop
+    end
+  end
+end
