9494d72e9facc1a150c4395e684cddd279cc9f3e
Bulat Shakirzyanov <mallluhuct@gmail.com>
2012-05-20 10:33:39 -0700

    add assertions to loop factory methods

--- lib/uv/loop.rb
+++ lib/uv/loop.rb
@@ -15,7 +15,7 @@ module UV
       allocate.tap { |i| i.send(:initialize, FFI::AutoPointer.new(pointer, UV.method(:loop_delete))) }
     end
 
-    include Resource
+    include Resource, Assertions
 
     def initialize(pointer)
       @pointer = pointer
@@ -47,6 +47,7 @@ module UV
 
     def last_error
       err = UV.last_error(@pointer)
+
       Error.const_get(UV.err_name(err).to_sym).new(UV.strerror(err))
     end
 
@@ -54,48 +55,63 @@ module UV
 
     def timer
       timer_ptr = UV.create_handle(:uv_timer)
+
       check_result! UV.timer_init(@pointer, timer_ptr)
       Timer.new(self, timer_ptr)
     end
 
     def tcp
       tcp_ptr = UV.create_handle(:uv_tcp)
+
       check_result! UV.tcp_init(@pointer, tcp_ptr)
       TCP.new(self, tcp_ptr)
     end
 
     def tty(io, readable = false)
+      fileno = io.fileno
+
+      assert_type(Integer, fileno, "io#fileno must return an integer file descriptor, #{fileno.inspect} given")
+      assert_boolean(readable)
+
       tty_ptr = UV.create_handle(:uv_tty)
-      check_result! UV.tty_init(@pointer, tty_ptr, io.fileno, readable ? 1 : 0)
+
+      check_result! UV.tty_init(@pointer, tty_ptr, fileno, readable ? 1 : 0)
       TTY.new(self, tty_ptr)
     end
 
     def pipe(ipc = false)
+      assert_boolean(ipc, "ipc must be a Boolean")
+
       pipe_ptr = UV.create_handle(:uv_pipe)
+
       check_result! UV.pipe_init(@pointer, pipe_ptr, ipc ? 1 : 0)
       Pipe.new(self, pipe_ptr)
     end
 
     def prepare
       prepare_ptr = UV.create_handle(:uv_prepare)
+
       check_result! UV.prepare_init(@pointer, prepare_ptr)
       Prepare.new(self, prepare_ptr)
     end
 
     def check
       check_ptr = UV.create_handle(:uv_check)
+
       check_result! UV.prepare_init(@pointer, check_ptr)
       Check.new(self, check_ptr)
     end
 
     def idle
       idle_ptr = UV.create_handle(:uv_idle)
+
       check_result! UV.idle_init(@pointer, idle_ptr)
       Idle.new(self, idle_ptr)
     end
 
     def async(&block)
       async_ptr = UV.create_handle(:uv_async)
+
       check_result! UV.async_init(@pointer, async_ptr)
       Async.new(self, async_ptr, &block)
     end
