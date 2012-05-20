2fb0b5144b44b0dccd950f1ebd89f0f5896bd93e
Bulat Shakirzyanov <mallluhuct@gmail.com>
2012-05-20 01:13:28 -0700

    add check spec

---
+++ spec/uv/check_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe UV::Check do
+  let(:handle_name) { :check }
+  let(:loop) { double() }
+  let(:pointer) { double() }
+  subject { UV::Check.new(loop, pointer) }
+
+  it_behaves_like 'a handle'
+
+  describe "#start" do
+    it "requires a block" do
+      expect { subject.start }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.check_start" do
+      UV.should_receive(:check_start).with(pointer, subject.method(:on_check))
+
+      subject.start {}
+    end
+  end
+
+  describe "#stop" do
+    it "calls UV.check_stop" do
+      UV.should_receive(:check_stop).with(pointer)
+
+      subject.stop
+    end
+  end
+end
