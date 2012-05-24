df1fd6f2dbeab530b06fbef6d977240b799a76be
Bulat Shakirzyanov <mallluhuct@gmail.com>
2012-05-24 15:53:51 -0500

    update brew formula

--- Formula/libuv.rb
+++ Formula/libuv.rb
@@ -4,30 +4,17 @@ class Libuv < Formula
   homepage 'https://github.com/joyent/libuv'
   head 'https://github.com/avalanche123/libuv.git'
 
-  if ARGV.build_head? and MacOS.xcode_version >= "4.3"
-    depends_on "libtool" => :build
-  end
-
-  def options
-    [
-      ['--with-dylib', 'Build with libuv.dylib, useful for FFI bindings']
-    ]
-  end
-
   def install
-    system "make"
+    system "svn", "co", "http://gyp.googlecode.com/svn/trunk", "build/gyp"
+    system './gyp_uv -f xcode -Dtarget_arch=ia64'
+    system 'xcodebuild', '-project', 'uv.xcodeproj', '-configuration', 'Release', '-target', 'All'
 
     cd 'include' do
       include.install Dir['*.h']
       (include+"uv-private").install Dir['uv-private/*.h']
     end
 
-    if ARGV.include?('--with-dylib')
-      system 'libtool', '-dynamic', '-framework', 'CoreServices', '-o', 'uv.1.dylib', 'uv.a', '-lc'
-      lib.install 'uv.1.dylib'
-      lib.install_symlink 'uv.1.dylib' => 'uv.dylib'
-    end
-
-    lib.install 'uv.a'
+    lib.install 'ext/libuv/build/Release/libuv.a'
+    lib.install 'ext/libuv/build/Release/libuv.dylib'
   end
 end
