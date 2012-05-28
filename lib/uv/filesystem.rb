338f74d33f951da5577a707a312a92a30a626b95
Bulat Shakirzyanov <mallluhuct@gmail.com>
2012-05-28 13:31:17 -0700

    more filesystem implementation

--- lib/uv/filesystem.rb
+++ lib/uv/filesystem.rb
@@ -1,21 +1,23 @@
 module UV
   class Filesystem
-    include Resource, Listener, Assertions
+    include Assertions, Resource, Listener
 
     def initialize(loop)
       @loop = loop
     end
 
-    def open(path, mode = 0, perm = 0, &block)
+    def open(path, flags = 0, mode = 0, &block)
       assert_block(block)
       assert_arity(2, block)
       assert_type(String, path, "path must be a String")
+      assert_type(Integer, flags, "flags must be an Integer")
       assert_type(Integer, mode, "mode must be an Integer")
-      assert_type(Integer, perm, "perm must be an Integer")
 
       @open_block = block
 
-      UV.fs_open(loop.to_ptr, UV.create_request(:uv_fs), mode, perm, callback(:on_open))
+      check_result! UV.fs_open(loop.to_ptr, UV.create_request(:uv_fs), path, flags, mode, callback(:on_open))
+
+      self
     end
 
     def unlink(path, &block)
@@ -25,18 +27,22 @@ module UV
 
       @unlink_block = block
 
-      UV.fs_unlink(loop.to_ptr, UV.create_request(:uv_fs), path, callback(:on_unlink))
+      check_result! UV.fs_unlink(loop.to_ptr, UV.create_request(:uv_fs), path, callback(:on_unlink))
+
+      self
     end
 
-    def mkdir(path, perm = 0777, &block)
+    def mkdir(path, mode = 0777, &block)
       assert_block(block)
       assert_arity(1, block)
       assert_type(String, path, "path must be a String")
-      assert_type(Integer, perm, "perm must be an Integer")
+      assert_type(Integer, mode, "mode must be an Integer")
 
       @mkdir_block = block
 
-      UV.fs_mkdir(loop.to_ptr, UV.create_request(:uv_fs), path, perm, callback(:on_mkdir))
+      check_result! UV.fs_mkdir(loop.to_ptr, UV.create_request(:uv_fs), path, mode, callback(:on_mkdir))
+
+      self
     end
 
     def rmdir(path, &block)
@@ -46,37 +52,137 @@ module UV
 
       @rmdir_block = block
 
-      UV.fs_unlink(loop.to_ptr, UV.create_request(:uv_fs), path, callback(:on_rmdir))
+      check_result! UV.fs_rmdir(loop.to_ptr, UV.create_request(:uv_fs), path, callback(:on_rmdir))
+
+      self
     end
 
-    def readdir(path, flags, &block)
+    def readdir(path, &block)
+      assert_block(block)
+      assert_arity(2, block)
+      assert_type(String, path, "path must be a String")
+
+      @readdir_block = block
+
+      check_result! UV.fs_readdir(loop.to_ptr, UV.create_request(:uv_fs), path, 0, callback(:on_readdir))
+
+      self
     end
 
     def stat(path, &block)
+      assert_block(block)
+      assert_arity(2, block)
+      assert_type(String, path, "path must be a String")
+
+      @stat_block = block
+
+      check_result! UV.fs_stat(loop.to_ptr, UV.create_request(:uv_fs), path, callback(:on_stat))
+
+      self
     end
 
-    def rename(path, new_path, &block)
+    def rename(old_path, new_path, &block)
+      assert_block(block)
+      assert_arity(1, block)
+      assert_type(String, old_path, "old_path must be a String")
+      assert_type(String, new_path, "new_path must be a String")
+
+      @rename_block = block
+
+      check_result! UV.fs_rename(loop.to_ptr, UV.create_request(:uv_fs), old_path, new_path, callback(:on_rename))
+
+      self
     end
 
     def chmod(path, mode, &block)
+      assert_block(block)
+      assert_arity(1, block)
+      assert_type(String, path, "path must be a String")
+      assert_type(Integer, mode, "mode must be an Integer")
+
+      @chmod_block = block
+
+      check_result! UV.fs_chmod(loop.to_ptr, UV.create_request(:uv_fs), path, mode, callback(:on_chmod))
+
+      self
     end
 
     def utime(path, atime, mtime, &block)
+      assert_block(block)
+      assert_arity(1, block)
+      assert_type(String, path, "path must be a String")
+      assert_type(Integer, atime, "atime must be an Integer")
+      assert_type(Integer, mtime, "mtime must be an Integer")
+
+      @utime_block = block
+
+      check_result! UV.fs_utime(loop.to_ptr, UV.create_request(:uv_fs), path, atime, mtime, callback(:on_utime))
+
+      self
     end
 
     def lstat(path, &block)
+      assert_block(block)
+      assert_arity(2, block)
+      assert_type(String, path, "path must be a String")
+
+      @lstat_block = block
+
+      check_result! UV.fs_lstat(loop.to_ptr, UV.create_request(:uv_fs), path, callback(:on_lstat))
+
+      self
     end
 
-    def link(path, new_path, &block)
+    def link(old_path, new_path, &block)
+      assert_block(block)
+      assert_arity(1, block)
+      assert_type(String, old_path, "old_path must be a String")
+      assert_type(String, new_path, "new_path must be a String")
+
+      @link_block = block
+
+      check_result! UV.fs_link(loop.to_ptr, UV.create_request(:uv_fs), old_path, new_path, callback(:on_link))
+
+      self
     end
 
-    def symlink(path, new_path, flags, &block)
+    def symlink(old_path, new_path, &block)
+      assert_block(block)
+      assert_arity(1, block)
+      assert_type(String, old_path, "old_path must be a String")
+      assert_type(String, new_path, "new_path must be a String")
+
+      @symlink_block = block
+
+      check_result! UV.fs_symlink(loop.to_ptr, UV.create_request(:uv_fs), old_path, new_path, 0, callback(:on_symlink))
+
+      self
     end
 
     def readlink(path, &block)
+      assert_block(block)
+      assert_arity(2, block)
+      assert_type(String, path, "path must be a String")
+
+      @readlink_block = block
+
+      check_result! UV.fs_readlink(loop.to_ptr, UV.create_request(:uv_fs), path, callback(:on_readlink))
+
+      self
     end
 
     def chown(path, uid, gid, &block)
+      assert_block(block)
+      assert_arity(1, block)
+      assert_type(String, path, "path must be a String")
+      assert_type(Integer, uid, "uid must be an Integer")
+      assert_type(Integer, gid, "gid must be an Integer")
+
+      @chown_block = block
+
+      check_result! UV.fs_chown(loop.to_ptr, UV.create_request(:uv_fs), path, uid, gid, callback(:on_chown))
+
+      self
     end
 
     # Watcher for a path
@@ -97,23 +203,147 @@ module UV
 
     def on_unlink(req)
       e = check_result(UV.fs_req_result(req))
+
       UV.fs_req_cleanup(req)
       UV.free(req)
+
       @unlink_block.call(e)
     end
 
     def on_mkdir(req)
       e = check_result(UV.fs_req_result(req))
+
       UV.fs_req_cleanup(req)
       UV.free(req)
+
       @mkdir_block.call(e)
     end
 
     def on_rmdir(req)
       e = check_result(UV.fs_req_result(req))
+
       UV.fs_req_cleanup(req)
       UV.free(req)
+
       @rmdir_block.call(e)
+    end
+
+    def on_readdir(req)
+      e = check_result(UV.fs_req_result(req))
+
+      unless e
+        string_ptr = UV.fs_req_pointer(req)
+        files = string_ptr.null? ? [] : string_ptr.read_string().split("\0")
+      end
+
+      UV.fs_req_cleanup(req)
+      UV.free(req)
+
+      @readdir_block.call(e, files)
+    end
+
+    def on_stat(req)
+      e = check_result(UV.fs_req_result(req))
+
+      unless e
+        uv_stat    = UV.fs_req_stat(req)
+        uv_members = uv_stat.members
+
+        values = Stat.members.map { |k| uv_members.include?(k) ? uv_stat[k] : nil }
+
+        stat = Stat.new(*values)
+      end
+
+      UV.fs_req_cleanup(req)
+      UV.free(req)
+
+      @stat_block.call(e, stat)
+    end
+
+    def on_rename(req)
+      e = check_result(UV.fs_req_result(req))
+
+      UV.fs_req_cleanup(req)
+      UV.free(req)
+
+      @rename_block.call(e)
+    end
+
+    def on_chmod(req)
+      e = check_result(UV.fs_req_result(req))
+
+      UV.fs_req_cleanup(req)
+      UV.free(req)
+
+      @chmod_block.call(e)
+    end
+
+    def on_utime(req)
+      e = check_result(UV.fs_req_result(req))
+
+      UV.fs_req_cleanup(req)
+      UV.free(req)
+
+      @utime_block.call(e)
+    end
+
+    def on_lstat(req)
+      e = check_result(UV.fs_req_result(req))
+
+      unless e
+        uv_stat    = UV.fs_req_stat(req)
+        uv_members = uv_stat.members
+
+        values = Stat.members.map { |k| uv_members.include?(k) ? uv_stat[k] : nil }
+
+        stat = Stat.new(*values)
+      end
+
+      UV.fs_req_cleanup(req)
+      UV.free(req)
+
+      @lstat_block.call(e, stat)
+    end
+
+    def on_link(req)
+      e = check_result(UV.fs_req_result(req))
+
+      UV.fs_req_cleanup(req)
+      UV.free(req)
+
+      @link_block.call(e)
+    end
+
+    def on_symlink(req)
+      e = check_result(UV.fs_req_result(req))
+
+      UV.fs_req_cleanup(req)
+      UV.free(req)
+
+      @symlink_block.call(e)
+    end
+
+    def on_readlink(req)
+      e = check_result(UV.fs_req_result(req))
+
+      unless e
+        string_ptr = UV.fs_req_pointer(req)
+        path = string_ptr.read_string() unless string_ptr.null?
+      end
+
+      UV.fs_req_cleanup(req)
+      UV.free(req)
+
+      @readlink_block.call(e, path)
+    end
+
+    def on_chown(req)
+      e = check_result(UV.fs_req_result(req))
+
+      UV.fs_req_cleanup(req)
+      UV.free(req)
+
+      @chown_block.call(e)
     end
   end
 end
---
+++ spec/uv/filesystem_spec.rb
@@ -0,0 +1,249 @@
+require 'spec_helper'
+
+describe UV::Filesystem do
+  let(:loop) { double() }
+  let(:loop_pointer) { double() }
+  subject { UV::Filesystem.new(loop) }
+
+  before(:each) do
+    loop.stub(:to_ptr) { loop_pointer }
+  end
+
+  describe "#open" do
+    let(:path) { "/tmp/file" }
+    let(:mode) { 0755 }
+    let(:flags) { File::CREAT | File::EXCL | File::APPEND }
+    let(:open_request) { double() }
+
+    it "requires a block" do
+      expect { subject.open(path, flags, mode) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_open" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(open_request)
+      UV.should_receive(:fs_open).with(loop_pointer, open_request, path, flags, mode, subject.method(:on_open))
+
+      subject.open(path, flags, mode) { |e, file| }
+    end
+  end
+
+  describe "#unlink" do
+    let(:path) { "/tmp/file" }
+    let(:unlink_request) { double() }
+
+    it "requires a block" do
+      expect { subject.unlink(path) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_unlink" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(unlink_request)
+      UV.should_receive(:fs_unlink).with(loop_pointer, unlink_request, path, subject.method(:on_unlink))
+
+      subject.unlink(path) { |e| }
+    end
+  end
+
+  describe "#mkdir" do
+    let(:path) { "/tmp/dir" }
+    let(:mode) { 0777 }
+    let(:mkdir_request) { double() }
+
+    it "requires a block" do
+      expect { subject.mkdir(path) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_mkdir" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(mkdir_request)
+      UV.should_receive(:fs_mkdir).with(loop_pointer, mkdir_request, path, mode, subject.method(:on_mkdir))
+
+      subject.mkdir(path, mode) { |e| }
+    end
+  end
+
+  describe "#rmdir" do
+    let(:path) { "/tmp/dir" }
+    let(:rmdir_request) { double() }
+
+    it "requires a block" do
+      expect { subject.rmdir(path) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_rmdir" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(rmdir_request)
+      UV.should_receive(:fs_rmdir).with(loop_pointer, rmdir_request, path, subject.method(:on_rmdir))
+
+      subject.rmdir(path) { |e| }
+    end
+  end
+
+  describe "#readdir" do
+    let(:path) { '/tmp' }
+    let(:readdir_request) { double() }
+
+    it "requires a block" do
+      expect { subject.readdir(path) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_readdir" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(readdir_request)
+      UV.should_receive(:fs_readdir).with(loop_pointer, readdir_request, path, 0, subject.method(:on_readdir))
+
+      subject.readdir(path) { |e, files| }
+    end
+  end
+
+  describe "#stat" do
+    let(:path) { '/tmp/filename' }
+    let(:stat_request) { double() }
+
+    it "requires a block" do
+      expect { subject.stat(path) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_stat" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(stat_request)
+      UV.should_receive(:fs_stat).with(loop_pointer, stat_request, path, subject.method(:on_stat))
+
+      subject.stat(path) { |e, stat| }
+    end
+  end
+
+  describe "#rename" do
+    let(:old_path) { '/tmp/old_file' }
+    let(:new_path) { '/tmp/new_file' }
+    let(:rename_request) { double() }
+
+    it "requires a block" do
+      expect { subject.rename(old_path, new_path) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_rename" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(rename_request)
+      UV.should_receive(:fs_rename).with(loop_pointer, rename_request, old_path, new_path, subject.method(:on_rename))
+
+      subject.rename(old_path, new_path) { |e| }
+    end
+  end
+
+  describe "#chmod" do
+    let(:path) { '/tmp/somepath' }
+    let(:mode) { 0755 }
+    let(:chmod_request) { double() }
+
+    it "requires a block" do
+      expect { subject.chmod(path, mode) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_chmod" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(chmod_request)
+      UV.should_receive(:fs_chmod).with(loop_pointer, chmod_request, path, mode, subject.method(:on_chmod))
+
+      subject.chmod(path, mode) { |e| }
+    end
+  end
+
+  describe "#utime" do
+    let(:path) { '/tmp/filename' }
+    let(:atime) { 1291404900 } # 2010-12-03 20:35:00
+    let(:mtime) { 400497753 }  # 1982-09-10 11:22:33
+    let(:utime_request) { double() }
+
+    it "requires a block" do
+      expect { subject.utime(path, atime, mtime) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_utime" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(utime_request)
+      UV.should_receive(:fs_utime).with(loop_pointer, utime_request, path, atime, mtime, subject.method(:on_utime))
+
+      subject.utime(path, atime, mtime) { |e| }
+    end
+  end
+
+  describe "#lstat" do
+    let(:path) { '/tmp/filename' }
+    let(:lstat_request) { double() }
+
+    it "requires a block" do
+      expect { subject.lstat(path) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_lstat" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(lstat_request)
+      UV.should_receive(:fs_lstat).with(loop_pointer, lstat_request, path, subject.method(:on_lstat))
+
+      subject.lstat(path) { |e, stat| }
+    end
+  end
+
+  describe "#link" do
+    let(:old_path) { '/tmp/old_file' }
+    let(:new_path) { '/tmp/new_file' }
+    let(:link_request) { double() }
+
+    it "requires a block" do
+      expect { subject.link(old_path, new_path) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_link" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(link_request)
+      UV.should_receive(:fs_link).with(loop_pointer, link_request, old_path, new_path, subject.method(:on_link))
+
+      subject.link(old_path, new_path) { |e| }
+    end
+  end
+
+  describe "#symlink" do
+    let(:old_path) { '/tmp/old_file' }
+    let(:new_path) { '/tmp/new_file' }
+    let(:symlink_request) { double() }
+
+    it "requires a block" do
+      expect { subject.symlink(old_path, new_path) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_link" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(symlink_request)
+      UV.should_receive(:fs_symlink).with(loop_pointer, symlink_request, old_path, new_path, 0, subject.method(:on_symlink))
+
+      subject.symlink(old_path, new_path) { |e| }
+    end
+  end
+
+  describe "#readlink" do
+    let(:path) { '/tmp/symlink' }
+    let(:readlink_request) { double() }
+
+    it "requires a block" do
+      expect { subject.readlink(path) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_readlink" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(readlink_request)
+      UV.should_receive(:fs_readlink).with(loop_pointer, readlink_request, path, subject.method(:on_readlink))
+
+      subject.readlink(path) { |e, path| }
+    end
+  end
+
+  describe "#chown" do
+    let(:path) { '/tmp/chownable_file' }
+    let(:uid) { 0 }
+    let(:gid) { 0 }
+    let(:chown_request) { double() }
+
+    it "requires a block" do
+      expect { subject.chown(path, uid, gid) }.to raise_error(ArgumentError)
+    end
+
+    it "calls UV.fs_chown" do
+      UV.should_receive(:create_request).with(:uv_fs).and_return(chown_request)
+      UV.should_receive(:fs_chown).with(loop_pointer, chown_request, path, uid, gid, subject.method(:on_chown))
+
+      subject.chown(path, uid, gid) { |e| }
+    end
+  end
+
+  describe "#watch" do
+  end
+end
